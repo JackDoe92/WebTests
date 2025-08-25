e0137cb98fda1516373ef8c9a5dcd563e03b1a0c
Jack <jackdoe92@proton.me>
2025-08-25 19:55:16 +1200

    wallpaper

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const showUserStoryButton = document.getElementById('showUserStory');
-    const additionalUserStory = document.getElementById('additionalUserStory');
-
-    showUserStoryButton.addEventListener('click', function() {
-        additionalUserStory.style.display = 'block';
-    });
-});
